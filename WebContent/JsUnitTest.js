2bf39835e43c44d7b2ca56acbdef7d203cdde56d
wbecker <wbecker@07fa3454-db33-11dd-9f21-5bfbb98aebe5>
2009-01-05 15:31:05 +0000

    removed files that are copied into their location

--- WebContent/JsUnitTest.js
+++
@@ -1,9 +0,0 @@
-Jsoak.JsUnitTest=function()
-{
-  var pub={};
-  /** Left empty */
-  pub.setup=function(){/* Left empty */};
-  /** Left empty */
-  pub.teardown=function(){/* Left empty */};
-  return pub;
-};
