3ebc81b1abbf460a6cec8a41c97c43c9000b0355
wbecker <wbecker@07fa3454-db33-11dd-9f21-5bfbb98aebe5>
2010-03-25 15:11:30 +0000

    Handle asynchronous tests

--- js_src/Jsoak.js
+++ js_src/Jsoak.js
@@ -105,66 +105,127 @@ var JsoakClass=function()
       ex.testSuiteName = test.testSuiteName;
       throw ex;
     }
+    var counter = {};
+    counter.amountStarted = 0;
+    counter.amountFinished = 0;
+    var testFinished = function () {
+      counter.amountFinished++;
+    };
     for(methodName in test)
     {
       if(prv.isMethodATest(test, methodName))
       {
+        counter.amountStarted++;
   	    try 
   	    {
-          prv.performTest(test.testSuiteName, test[methodName], methodName);
+          prv.performTest(test.testSuiteName, test[methodName], methodName, testFinished);
   	    } 
-		catch (ex) 
-		{
-		  console.log(ex);
-		}
+        catch (ex) 
+        {
+          console.log(ex);
+        }
       }
     }
-    try
-    {
-      test[prv.teardownString]();
-    }
-    catch(ex) 
-    {
-      ex.message = "Exception in test teardown: "+ex.message;
-      ex.testSuiteName = test.testSuiteName;
-      throw ex;
-    }
+    prv.finishTests(counter);
   };
-  
+  prv.finishTests = function (counter) {
+    return function () {
+      if (amountStarted === amountFinished) {
+        try {
+          test[prv.teardownString]();
+        }
+        catch(ex) 
+        {
+          ex.message = "Exception in test teardown: "+ex.message;
+          ex.testSuiteName = test.testSuiteName;
+          throw ex;
+        }
+      }
+      else {
+        ddl.setTimeout(prv.finishTests(counter), 1000)
+      }
+    };
+  };
   prv.isMethodATest = function (test, methodName) {
     return (methodName.substring(0,prv.testPrefix.length)===prv.testPrefix)
            && (typeof test[methodName] === "function");
   };
   
-  prv.performTest = function (testSuiteName, test, methodName) {
+  prv.performTest = function (testSuiteName, test, methodName, testFinished) {
     try
     {
-      test();
-      prv.bridge.counter.addSuccess(testSuiteName+"."+methodName, function () {});
-      console.log(methodName + " succeeded.")
+      var asyncHandler = function () {
+        var asyncHandler_pub = {}, asyncHandler_prv = {};
+        asyncHandler_prv.tests = [];
+        asyncHandler_prv.timeWaited = 0;
+        asyncHandler_pub.addTest = function (timeToWait, test) {
+          asyncHandler_prv.tests.push([timeToWait, test]);
+        };
+        asyncHandler_pub.runTests = function () {
+          if (asyncHandler_prv.tests.length > 0) {
+            var testObj = asyncHandler_prv.tests[0];
+            asyncHandler_prv.tests.shift();
+            var timeToWait = testObj[0];
+            var test = testObj[1];
+            if (timeToWait > asyncHandler_prv.timeWaited) {
+              setTimeout(asyncHandler_prv.runTest(test), timeToWait - asyncHandler_prv.timeWaited);
+              asyncHandler_prv.timeWaited = timeToWait;
+            }
+            else {
+              asyncHandler_prv.runTest(test);
+            }
+          }
+          else {            
+            prv.bridge.counter.addSuccess(testSuiteName+"."+methodName, function () {});
+            testFinished();
+          }
+        };
+        asyncHandler_prv.runTest = function (test) {
+          return function () {
+            try {
+              test();
+              asyncHandler_pub.runTests();
+            }
+            catch (e) {
+              prv.handleError(e, testSuiteName, methodName);
+            }
+          }
+        }
+        return asyncHandler_pub;
+      }();
+      test(asyncHandler);
+      asyncHandler.runTests();
     }
-    catch(e)
+    catch (e)
     {
-      var errorMessage;
-      if(e.jsUnitMessage) {
-    	errorMessage = e.comment+" - "+e.jsUnitMessage;
-      } 
-      else {
-    	if(e.fileName && e.lineNumber) {
-    	  errorMessage = "[" + e.fileName + "@" + e.lineNumber + "] ";
-    	}
-    	else if(e.fileName) {
-      	  errorMessage = "[" + e.fileName + "] ";
-      	}
-    	else if(e.lineNumber) {
-      	  errorMessage = "[ line: " + e.lineNumber + "] ";
-      	}
-    	errorMessage = errorMessage + e.name+": "+e.message;
-      }
-      prv.bridge.counter.addFailure(testSuiteName+"."+methodName, errorMessage, function(){});
-      console.log(methodName+" failed. Reason1: "+ e.jsUnitMessage);
-      console.log(e)
+      prv.handleError(e, testSuiteName, methodName);
     }   
+  };
+  prv.asyncTest = function () {
+    return function (timeToWait, asyncTests) {
+      setTimeout(asyncTests, timeToWait);
+    };
+  };
+  prv.handleError = function (e, testSuiteName, methodName) {
+    var errorMessage;
+    if(e.jsUnitMessage) {
+    errorMessage = e.comment+" - "+e.jsUnitMessage;
+    } 
+    else {
+    if(e.fileName && e.lineNumber) {
+      errorMessage = "[" + e.fileName + "@" + e.lineNumber + "] ";
+    }
+    else if(e.fileName) {
+      errorMessage = "[" + e.fileName + "] ";
+    }
+    else if(e.lineNumber) {
+      errorMessage = "[ line: " + e.lineNumber + "] ";
+    }
+    errorMessage = errorMessage + e.name+": "+e.message;
+    }
+    prv.bridge.counter.addFailure(testSuiteName+"."+methodName, errorMessage, function(){});
+    console.log(methodName+" failed. Reason1: "+ e.jsUnitMessage);
+    console.log(e)
   };
   return pub;
 };
